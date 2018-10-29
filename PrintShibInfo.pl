ddd6799f5c277d8ca379bc8f778030b6364342ab
Jim Vanderveen <jim.vanderveen@ucop.edu>
2018-10-29 10:54:06 -0700

    Generalize PrintShibInfo, perl cleanup

--- PrintShibInfo.pl
+++ PrintShibInfo.pl
@@ -1,65 +1,80 @@
-#! /usr/bin/perl
+#!/usr/bin/perl
+# @(#) Test shibboleth IdP configuration via CGI
 
-print "Content-type: text/html; charset=ISO-8859-1\n\n";
-print <<"-30-";
-<title>DMP Tool Shibboleth Attribute Test</title>
-<h1>DMP Tool Shibboleth Attribute Test</h1>
+my $HTML_HEADER=<<"END_HEADER";
+Content-type: text/html; charset=iso-8859-1
+
+<title>Shibboleth Attribute Test</title>
+<h1>Shibboleth Attribute Test - $ENV{"SERVER_NAME"}</h1>
 <p>
-This CGI checks to see if this DMP Tool server is receiving the
+This CGI checks to see if this server is receiving the
 attributes from your Identity Provider that it requires to
-successfully authenticate you as a user of the the DMP Tool.
+successfully authenticate you as a user of this resource.
 </p>
--30-
+END_HEADER
 
+#----------------------------------------------------------------------------
+
+my $eppn;
+my $mail;
 $eppn = $mail = '';
 
-for $k(sort(keys(%ENV))) { 
-   next unless ($headervalue = $ENV{$k});
-   next unless ($k =~ /^(HTTP_EPPN|HTTP_MAIL|Shib|[a-z])/);
-   $label = "<b>$k:</b> ";
+print $HTML_HEADER;
 
-   # Probably not necessary, but just in case.....
-   $headervalue =~ tr/\000-\037\177-\377//d;
-   $headervalue =~ s/</&lt;/g;
-   $headervalue =~ s/>/&gt;/g;
+KEY_LOOP:
+for my $k (sort(keys(%ENV))) {
+    # Skip keys with empty values.
+    $headervalue = $ENV{$k}
+      or next KEY_LOOP;
 
-   if (($k eq 'HTTP_EPPN') && ($headervalue =~ /.+\@.+\..+/)) {
-		$eppn = $headervalue;
-   } elsif (($k eq 'HTTP_MAIL') && ($headervalue =~ /.+\@.+\..+/)) {
-		$mail = $headervalue;
-   } else {
-	 $printInfo .= $label . $headervalue . "\n";
-   }
+    # Skip environment variables we (probably) don't care about.
+    $k =~ /^(HTTP_EPPN|HTTP_MAIL|Shib|[a-z])/
+      or next KEY_LOOP;
+
+    $label = "<b>$k:</b> ";
+
+    # Probably not necessary, but just in case.....
+    $headervalue =~ tr/\000-\037\177-\377//d;
+    $headervalue =~ s/</&lt;/g;
+    $headervalue =~ s/>/&gt;/g;
+
+    if (($k eq 'HTTP_EPPN') && ($headervalue =~ /.+\@.+\..+/)) {
+      $eppn = $headervalue;
+    } elsif (($k eq 'HTTP_MAIL') && ($headervalue =~ /.+\@.+\..+/)) {
+      $mail = $headervalue;
+    } else {
+      $printInfo .= $label . $headervalue . "\n";
+    }
 }
 
-if ($eppn ne '' && $mail ne '') {
-	print <<"-30-";
-	<h2>Success!</h2>
-	The attributes required (eppn and mail) are being successfully received by this SP.
-	<ul>
-	<li> eppn: $eppn
-	<li> mail: $mail
-	</ul>
--30-
+if ($eppn && $mail) {
+    print <<"END_SUCCESS_MSG";
+        <h2>Success!</h2>
+        The attributes required (eppn and mail) are being successfully received by this SP.
+        <ul>
+        <li> eppn: $eppn
+        <li> mail: $mail
+        </ul>
+END_SUCCESS_MSG
 } else {
-	print <<"-30-";
-	<h2>Failure!</h2>
-	All the attributes required (eppn and mail) are NOT being successfully received by this SP.
-	<ul>
-	<li> eppn: $eppn
-	<li> mail: $mail
-	</ul>
-	Please review your Identity Provider logs and attribute release rules
-	to determine what might be preventing releasing all the necessary 
-	attributes to this SP.
--30-
+    print <<"END_FAILURE_MSG";
+        <h2>Failure!</h2>
+        All the attributes required (eppn and mail) are NOT being successfully received by this SP.
+        <ul>
+        <li> eppn: $eppn
+        <li> mail: $mail
+        </ul>
+        Please review your Identity Provider logs and attribute release rules
+        to determine what might be preventing releasing all the necessary
+        attributes to this SP.
+END_FAILURE_MSG
 }
 
-print <<"-30-";
-<h3>All the Shibboleth-related information being received by this SP</h3>		
+print <<"HTML_FOOTER";
+<h3>All the Shibboleth-related information being received by this SP</h3>
 <pre>
 $printInfo
 </pre>
--30-
+HTML_FOOTER
 
 exit;
